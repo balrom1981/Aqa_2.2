7313c25d21825ebb8583ae5f2bb2fa9dc16d8dd8
balrom1981 <balrom@bk.ru>
2021-03-09 08:08:54 +0300

    Test changed

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -1,24 +1,46 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
+
+    @BeforeEach
+    void shouldCleanAndSetData(){
+        open("http://localhost:9999");
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.DAY_OF_YEAR, 10); // увеличиваем на 4 дня от текущей даты
+        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
+        String str = format1.format(c.getTime());
+        $("[data-test-id='date'] input").setValue(str);
+    }
+
     @Test
     void shouldAcceptInformation() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue("Сочи");
-        $("[data-test-id='date'] input").click();
-        $("[placeholder='Дата встречи']").clear();
-//        $(Selectors.withText("10.03.2021")).clear();
-        $("[data-test-id='date'] input").setValue("20.03.2021");
+        $("[data-test-id=city] input").setValue("Москва");
         $("[data-test-id='name'] input").setValue("Иванов Василий");
-        $("[data-test-id='phone'] input").setValue("+792000000000");
-
-
+        $("[data-test-id='phone'] input").setValue("+79200000000");
+        $("[data-test-id=agreement]").click();
+        $$("button").get(1).click();
+        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
     }
 
 }
