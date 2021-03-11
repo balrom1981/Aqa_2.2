36ce0e8f1315da51aca0f329bbea2a5c23830349
balrom1981 <balrom@bk.ru>
2021-03-11 14:43:26 +0300

    shouldAcceptInformation changed

--- src/test/java/ru/netology/web/DeliveryTest.java
+++ src/test/java/ru/netology/web/DeliveryTest.java
@@ -1,36 +1,31 @@
 package ru.netology.web;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
 
+
     @BeforeEach
     void shouldCleanAndSetData() {
         open("http://localhost:9999");
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [pattern='[0-9.]*']").sendKeys(Keys.BACK_SPACE);
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 10); // увеличиваем на 10 дней от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String str = format1.format(c.getTime());
-        $("[data-test-id='date'] input").setValue(str); // записываем дату в поле
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        String str = LocalDate.now().plusDays(10).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='date'] input").setValue(str);
     }
 
     @Test
@@ -40,7 +35,8 @@ class DeliveryTest {
         $("[data-test-id='phone'] input").setValue("+79200000000");
         $("[data-test-id=agreement]").click();
         $$("button").get(1).click();
-        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(14));
+        String data = $("[data-test-id='date'] input").getCssValue("value");
+        $("[class='notification__content']").shouldHave(exactText(data), Duration.ofSeconds(14));
     }
 
     @Test
